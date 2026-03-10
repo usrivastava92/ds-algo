feca2a8a41656475f0b1c41055d166c58261fbea
Utkarsh Srivastava <usrivastava92@gmail.com>
2026-03-10 17:02:06 +0530

    Appmod/java upgrade 20260310105433 (#8)
    * Migrate project to Gradle Kotlin DSL and update CI to use gradlew + JDK21

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ![GitHub Workflow Status (event)](https://img.shields.io/github/workflow/status/usrivastava92/ds-algo/Java%20CI%20with%20Maven)
 
 This repository contains solution of data structures and algorithm problem from multiple online platforms
+
+*Requires Java 21 or later to compile and run.*
---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    java
+}
+
+allprojects {
+    group = "com.coding"
+    version = "1.0"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "java")
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    dependencies {
+        compileOnly("org.projectlombok:lombok:1.18.42")
+        annotationProcessor("org.projectlombok:lombok:1.18.42")
+        implementation("org.apache.commons:commons-lang3:3.20.0")
+        implementation("org.apache.poi:poi:5.5.1")
+        implementation("org.apache.poi:poi-ooxml:5.5.1")
+        implementation("org.slf4j:slf4j-api:2.0.17")
+        implementation("junit:junit:4.13.2")
+        testImplementation("junit:junit:4.13.2")
+        testCompileOnly("org.projectlombok:lombok:1.18.42")
+        testAnnotationProcessor("org.projectlombok:lombok:1.18.42")
+        if (project.name != "data-structures") {
+            implementation(project(":data-structures"))
+        }
+    }
+}
